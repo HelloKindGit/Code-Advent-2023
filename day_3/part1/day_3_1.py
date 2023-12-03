98a8ec7674d29928cecc54a59833b5f3b3ee08fd
HelloKindGit <dominik.wache@web.de>
2023-12-03 15:27:18 +0100

    deleted some redundancies and smaller changes

--- day_3/part1/day_3_1.py
+++ day_3/part1/day_3_1.py
@@ -1,40 +1,29 @@
-from collections import defaultdict
-
 def read_engine_schematic(file_path):
-    data = open(file_path).read().strip()
-    return [[c for c in line] for line in data.split('\n')]
+    with open(file_path) as f:
+        return [list(line.strip()) for line in f]
 
 def calculate_result(grid):
-    rows = len(grid)
-    cols = len(grid[0])
+    rows, cols = len(grid), len(grid[0])
 
     result = 0
-    adjacent_numbers = defaultdict(list)
 
     for r in range(rows):
-        gears = set()  # positions of '*' characters next to the current number
         current_number = 0
         has_part = False
-
         for c in range(cols + 1):
             if c < cols and grid[r][c].isdigit():
                 current_number = current_number * 10 + int(grid[r][c])
-                for dr in [-1, 0, 1]:
-                    for dc in [-1, 0, 1]:
-                        if 0 <= r + dr < rows and 0 <= c + dc < cols:
-                            char = grid[r + dr][c + dc]
+                for row_offset in [-1, 0, 1]:
+                    for col_offset in [-1, 0, 1]:
+                        if 0 <= r + row_offset < rows and 0 <= c + col_offset < cols:
+                            char = grid[r + row_offset][c + col_offset]
                             if not char.isdigit() and char != '.':
                                 has_part = True
-                            if char == '*':
-                                gears.add((r + dr, c + dc))
             elif current_number > 0:
-                for gear in gears:
-                    adjacent_numbers[gear].append(current_number)
                 if has_part:
                     result += current_number
                 current_number = 0
                 has_part = False
-                gears = set()
 
     return result
 
--- day_3/part2/day_3_2.py
+++ day_3/part2/day_3_2.py
@@ -1,12 +1,11 @@
 from collections import defaultdict
 
 def read_engine_schematic(file_path):
-    data = open(file_path).read().strip()
-    return [[c for c in line] for line in data.split('\n')]
+    with open(file_path) as f:
+        return [list(line.strip()) for line in f]
 
 def calculate_result(grid):
-    rows = len(grid)
-    cols = len(grid[0])
+    rows, cols = len(grid), len(grid[0])
 
     result = 0
     adjacent_numbers = defaultdict(list)
@@ -18,19 +17,19 @@ def calculate_result(grid):
         for c in range(cols + 1):
             if c < cols and grid[r][c].isdigit():
                 current_number = current_number * 10 + int(grid[r][c])
-                for dr in [-1, 0, 1]:
-                    for dc in [-1, 0, 1]:
-                        if 0 <= r + dr < rows and 0 <= c + dc < cols:
-                            char = grid[r + dr][c + dc]
+                for row_offset in [-1, 0, 1]:
+                    for col_offset in [-1, 0, 1]:
+                        if 0 <= r + row_offset < rows and 0 <= c + col_offset < cols:
+                            char = grid[r + row_offset][c + col_offset]
                             if char == '*':
-                                gears.add((r + dr, c + dc))
+                                gears.add((r + row_offset, c + col_offset))
             elif current_number > 0:
                 for gear in gears:
                     adjacent_numbers[gear].append(current_number)
                 current_number = 0
                 gears = set()
 
-    for k, v in adjacent_numbers.items():
+    for _, v in adjacent_numbers.items():
         if len(v) == 2:
             result += v[0] * v[1]
 
