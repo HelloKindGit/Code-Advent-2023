1b96c2a74461918586ee9e9534f9dd034fc75872
HelloKindGit <dominik.wache@web.de>
2023-12-09 21:13:17 +0100

    Day 9

---
+++ day_9/part1/day_9_1.py
@@ -0,0 +1,35 @@
+def generate_sequences(history):
+    differences = [b - a for a, b in zip(history, history[1:])]
+    sequences = [history, differences]
+    non_zero_check = sum(1 for i in differences if i)
+
+    while non_zero_check:
+        next_differences = [b - a for a, b in zip(differences, differences[1:])]
+        sequences.append(next_differences)
+        differences = next_differences
+        non_zero_check = sum(1 for i in differences if i)
+
+    return sequences
+
+def extrapolate_values(sequences):
+    value = 0
+    for sequence in sequences[::-1]:
+        value += sequence[-1]
+
+    return value
+
+def extrapolate_next_value(history):
+    sequences = generate_sequences(history)
+    return extrapolate_values(sequences)
+
+def calculate_histories():
+    file_path = "day_9/part1/input.txt"  # 'day_9/part1/test.txt'
+    with open(file_path, 'r') as file:
+        data = [line.strip() for line in file]
+
+    sum_of_extrapolated_values = sum(extrapolate_next_value(list(map(int, line.split()))) for line in data)
+
+    return sum_of_extrapolated_values
+
+result = calculate_histories()
+print(result)
